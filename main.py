3b12be748e5396dfc00251410cd614d1b7af39d2
aabdimalik09-star <aabdimalik09@gmail.com>
2026-04-22 01:51:19 +0300

    complete asignment

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')# decorator function
 def home():#view function
-    return "hello world"
+    return render_template('index.html')
 
 
 #http://127.0.0.1:5000/products
